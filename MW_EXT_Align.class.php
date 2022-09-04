1c6532c1c829137f383c61226d3fec357b8fdc40
z17cx <mail@z17.cx>
2022-09-04 18:24:40 +0000

    2022-09-04 18:24:40

---
+++ MW_EXT_Align.class.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace MediaWiki\Extension\CMFStore;
+
+use OutputPage, Parser, PPFrame, Skin;
+
+/**
+ * Class MW_EXT_Align
+ */
+class MW_EXT_Align
+{
+  /**
+   * Get align.
+   *
+   * @param $id
+   *
+   * @return array
+   */
+  private static function getAlign($id)
+  {
+    $get = MW_EXT_Kernel::getJSON(__DIR__ . '/storage/align.json');
+    $out = $get['align'][$id] ?? [] ?: [];
+
+    return $out;
+  }
+
+  /**
+   * Get align type.
+   *
+   * @param $id
+   * @param $type
+   *
+   * @return string
+   */
+  private static function getType($id, $type)
+  {
+    $id = self::getAlign($id) ? self::getAlign($id) : '';
+    $out = $id[$type] ?? '' ?: '';
+
+    return $out;
+  }
+
+  /**
+   * Register tag function.
+   *
+   * @param Parser $parser
+   *
+   * @return bool
+   * @throws \MWException
+   */
+  public static function onParserFirstCallInit(Parser $parser)
+  {
+    $parser->setHook('align', [__CLASS__, 'onRenderTag']);
+
+    return true;
+  }
+
+  /**
+   * Render tag function.
+   *
+   * @param $input
+   * @param array $args
+   * @param Parser $parser
+   * @param PPFrame $frame
+   *
+   * @return string
+   */
+  public static function onRenderTag($input, array $args, Parser $parser, PPFrame $frame)
+  {
+    // Argument: id.
+    $getID = MW_EXT_Kernel::outClear($args['id'] ?? '' ?: '');
+    $outID = MW_EXT_Kernel::outNormalize($getID);
+
+    // Argument: type.
+    $getType = MW_EXT_Kernel::outClear($args['type'] ?? '' ?: '');
+    $outType = MW_EXT_Kernel::outNormalize($getType);
+
+    // Check note type, set error category.
+    if (!self::getAlign($outID) || !self::getType($outID, $outType)) {
+      $parser->addTrackingCategory('mw-ext-align-error-category');
+
+      return null;
+    }
+
+    // Get content.
+    $getContent = trim($input);
+    $outContent = $parser->recursiveTagParse($getContent, $frame);
+
+    // Out HTML.
+    $outHTML = '<div class="mw-ext-align mw-ext-align-' . $outID . '-' . $outType . '">' . $outContent . '</div>';
+
+    // Out parser.
+    $outParser = $outHTML;
+
+    return $outParser;
+  }
+
+  /**
+   * Load resource function.
+   *
+   * @param OutputPage $out
+   * @param Skin $skin
+   *
+   * @return bool
+   */
+  public static function onBeforePageDisplay(OutputPage $out, Skin $skin)
+  {
+    $out->addModuleStyles(['ext.mw.align.styles']);
+
+    return true;
+  }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Information
+
+Выравнивание текста в статье.
+
+## Install
+
+1. Загрузите папки и файлы в директорию `extensions/MW_EXT_Align`.
+2. В самый низ файла `LocalSettings.php` добавьте строку:
+
+```php
+wfLoadExtension( 'MW_EXT_Align' );
+```
+
+## Syntax
+
+```html
+<align id="[text|image]" type="[left|center|right]">[CONTENT]</align>
+```
+
